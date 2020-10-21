46c3a64d7191123a4753a5c860affc9820d641de
Iberthy Allano <iberthya@hotmail.com>
2020-10-20 23:40:22 -0300

    Projeto comparador de genomas humano/bacteria
    Adicionei a análise de crescimento da população brasileira

--- src/test_genoma.py
+++ src/test_genoma.py
@@ -1,12 +1,10 @@
 from Genomas import Genomas;
 
-entradaHumano = 'src/dadosGenoma/humano.fasta';
-saidaHumano = 'src/dadosGenoma/humano.html';
+entradaHumano = 'src/Database/DadosGenoma/humano.fasta';
+saidaHumano = 'src/Database/DadosGenoma/humano.html';
 
-entradaBacteria = 'src/dadosGenoma/bacteria.fasta';
-saidaBacteria = 'src/dadosGenoma/bacteria.html';
-
-
+entradaBacteria = 'src/Database/DadosGenoma/bacteria.fasta';
+saidaBacteria = 'src/Database/DadosGenoma/bacteria.html';
 
 h = Genomas(entradaHumano, saidaHumano);
 b = Genomas(entradaBacteria, saidaBacteria);
